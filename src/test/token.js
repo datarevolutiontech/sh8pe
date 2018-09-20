d49cfd0b78c2365faa83214e199b0c0abd589f5b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-09-21 08:54:35 +1000

    Clean up

--- src/test/token.js
+++ src/test/token.js
@@ -1,31 +1,28 @@
 const Token = artifacts.require("Sh8pe");
-//const assertJump = require("./assertJump.js");
-const tokenName = 'Sh8pe';
-const decimalUnits = 18;
-const tokenSymbol = 'INVOX';
 
-const durationTime = 28; //4 weeks
+const tokenName = 'Sh8pe Angel Token';
+const decimalUnits = 18;
+const tokenSymbol = 'SH8PE';
 
 contract('Sh8pe', function(accounts) {
   beforeEach(async function () {
     this.token = await Token.new();
   });
 
-  //describe erc20 tests
   describe("Test ERC20 properties", async function() {
-    it("should have symbol INVOX", async function () {
+    it("should have symbol SH8PE", async function () {
       const actual = await this.token.symbol();
-      assert.equal(actual, tokenSymbol, "Symbol should be Sh8pe");
+      assert.equal(actual, tokenSymbol, "Symbol should be SH8PE");
     });
 
-    it("should have name Invox", async function () {
+    it("should have name sh", async function () {
       const actual = await this.token.name();
-      assert.equal(actual, tokenName, "Name should be Sh8pe");
+      assert.equal(actual, tokenName, "Name should be Sh8pe Angel Token");
     });
 
-    it("should have total supply of 0", async function () {
+    it("should have total supply of 100000000", async function () {
       const balance = await this.token.balanceOf(accounts[0]);
-      assert.equal(balance.valueOf(), 0, "First account (owner) balance must be 0");
+      assert.equal(balance.valueOf(), 100000000, "First account (owner) balance must be 100000000");
     });
 
     it("should put 0 INVX to supply and in the first account", async function () {
